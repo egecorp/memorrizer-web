76cdd425ec6e49e2f7f8028fd781910ae981fbdc
Vadim Serednev <vserednevdev@gmail.com>
2023-06-18 16:13:31 +0300

    the first attempt design

--- exam.js
+++ exam.js
@@ -1,4 +1,4 @@
-class Exam{
+class Exam {
     #TaskText
     #LessonProgressProgressBarGreen
     #LessonProgressProgressBarRed
@@ -14,157 +14,139 @@ class Exam{
     ResultKnownWords
     ResultAttempts
 
-    constructor(lesson){
-        if (lesson){
+    constructor(lesson) {
+        if (lesson) {
             this.#sourceLesson = lesson
         }
 
         this.ResultName = lesson.Name
         this.ResultCount = lesson.Words.length
 
-        this.#TaskText = document.getElementById("TaskText");
-        this.#LessonProgressProgressBarGreen = document.getElementById("LessonProgressProgressBarGreen");
-        this.#LessonProgressProgressBarRed = document.getElementById("LessonProgressProgressBarRed");
-        this.#LessonAttempt = document.getElementById("LessonAttempt");
-        this.#LessonInput = document.getElementById("LessonInput");
+        this.#TaskText = document.getElementById('TaskText')
+        this.#LessonProgressProgressBarGreen = document.getElementById('LessonProgressProgressBarGreen')
+        this.#LessonProgressProgressBarRed = document.getElementById('LessonProgressProgressBarRed')
+        this.#LessonAttempt = document.getElementById('LessonAttempt')
+        this.#LessonInput = document.getElementById('LessonInput')
 
-        this.#ResultText = document.getElementById("ResultText");
-        this.#ResultSection = document.getElementById("ResultSection");
-
+        this.#ResultText = document.getElementById('ResultText')
+        this.#ResultSection = document.getElementById('ResultSection')
     }
 
+    Start() {
+        this.Words = [...this.#sourceLesson.Words]
+        this.Attempt = 0
 
-
-    Start(){
-        
-        this.Words = [...this.#sourceLesson.Words];
-        this.Attempt = 0;
-       
         this.NewRound(true)
-
-        
     }
 
-    NewRound(isFirstRound){
-        this.ResultAttempts = ++this.Attempt;
-        if (isFirstRound !== true){
-            this.Words = [...this.Failed];
+    NewRound(isFirstRound) {
+        this.ResultAttempts = ++this.Attempt
+        if (isFirstRound !== true) {
+            this.Words = [...this.Failed]
         }
-        this.Shuffle();
+        this.Shuffle()
 
-        this.Failed = [];
-        this.Done = [];
+        this.Failed = []
+        this.Done = []
 
-        this.CurrentWordPosition = 0;
-        this.CurrentWordIsFail = false;
+        this.CurrentWordPosition = 0
+        this.CurrentWordIsFail = false
         this.CurrentWord = this.Words[this.CurrentWordPosition]
         this.Render()
         this.#ResultSection.style.backgroundColor = 'wheat'
     }
 
-    SelectNext(){
-        this.CurrentWordIsFail = false;
-        if (++this.CurrentWordPosition > this.Words.length - 1){
-
-            if (this.Attempt === 1){
-                this.ResultKnownWords = this.Done.length;
+    SelectNext() {
+        this.CurrentWordIsFail = false
+        if (++this.CurrentWordPosition > this.Words.length - 1) {
+            if (this.Attempt === 1) {
+                this.ResultKnownWords = this.Done.length
             }
-            
 
-            if(this.Failed.length > 0){
+            if (this.Failed.length > 0) {
                 this.NewRound()
-            }
-            else {
+            } else {
                 Navigator.OpenLessonResult(this)
             }
             return
         }
-        
+
         this.CurrentWord = this.Words[this.CurrentWordPosition]
         this.Render()
     }
 
-    Render(doNotClearInput){
-
-        if (!this.CurrentWord){
-            this.#TaskText.innerText = '';
-            return;    
+    Render(doNotClearInput) {
+        if (!this.CurrentWord) {
+            this.#TaskText.innerText = ''
+            return
         }
-        this.#TaskText.innerText = this.CurrentWord.Title;
-        
-        let green = Math.round(100 *(this.Done.length / this.Words.length));
-        this.#LessonProgressProgressBarGreen.style.width = green + '%';
-        let red = Math.round(100 *(this.Failed.length / this.Words.length))
-        this.#LessonProgressProgressBarRed.style.width= red + '%';
-        
-        this.#LessonAttempt.innerText = 'Круг ' + this.Attempt;
-        
-        if (doNotClearInput !== true){
-            this.#LessonInput.value = '';
-        }
+        this.#TaskText.innerText = this.CurrentWord.Title
 
-        
+        let green = Math.round(100 * (this.Done.length / this.Words.length))
+        this.#LessonProgressProgressBarGreen.style.width = green + '%'
+        let red = Math.round(100 * (this.Failed.length / this.Words.length))
+        this.#LessonProgressProgressBarRed.style.width = red + '%'
+
+        this.#LessonAttempt.innerText = 'Круг ' + this.Attempt
+
+        if (doNotClearInput !== true) {
+            this.#LessonInput.value = ''
+        }
     }
 
-    Commit(){
-        if (!this.CurrentWord) return;
+    Commit() {
+        if (!this.CurrentWord) return
 
-        let word = (this.#LessonInput.value || "").toLowerCase().trim();
+        let word = (this.#LessonInput.value || '').toLowerCase().trim()
 
-        if (this.CurrentWord.Answer == word){
-            this.#ResultText.innerText = 'Правильно! ' + this.CurrentWord.Title + ' : ' +this.CurrentWord.Answer;
+        if (this.CurrentWord.Answer == word) {
+            this.#ResultText.innerText = 'Правильно! ' + this.CurrentWord.Title + ' : ' + this.CurrentWord.Answer
 
-            if (this.CurrentWordIsFail === false){
-                this.Done.push(this.CurrentWord);
+            if (this.CurrentWordIsFail === false) {
+                this.Done.push(this.CurrentWord)
             }
-            this.#ResultSection.style.backgroundColor = 'lightgreen'
-            
-            this.SelectNext();
-        } else {
+            this.#ResultSection.style.backgroundColor = '#d3fbdb'
 
-            for(var synonym of this.CurrentWord.Synonyms ){
+            this.SelectNext()
+        } else {
+            for (var synonym of this.CurrentWord.Synonyms) {
                 if (synonym === word) {
-                    this.#ResultText.innerText = 'Нужен синоним';
+                    this.#ResultText.innerText = 'Нужен синоним'
                     this.#ResultSection.style.backgroundColor = 'yellow'
-                    return;
+                    return
                 }
             }
 
-            this.#ResultText.innerText = 'Неправильно! ';
-            
-            if (this.CurrentWordIsFail !== true){
-                this.Failed.push(this.CurrentWord);
+            this.#ResultText.innerText = 'Неправильно! '
+
+            if (this.CurrentWordIsFail !== true) {
+                this.Failed.push(this.CurrentWord)
                 this.CurrentWordIsFail = true
             }
-            this.#ResultSection.style.backgroundColor = 'lightpink'
+            this.#ResultSection.style.backgroundColor = '#ffa9a9'
 
-            this.Render(true);
+            this.Render(true)
         }
-        
     }
 
     ShowTip() {
-        this.#ResultText.innerText = 'Подсказка: ! ' +  this.CurrentWord.Title + ' : ' +this.CurrentWord.Answer;
-        if (this.CurrentWordIsFail !== true){
-            this.Failed.push(this.CurrentWord);
+        this.#ResultText.innerText = 'Подсказка: ! ' + this.CurrentWord.Title + ' : ' + this.CurrentWord.Answer
+        if (this.CurrentWordIsFail !== true) {
+            this.Failed.push(this.CurrentWord)
             this.CurrentWordIsFail = true
         }
-        this.Render();
+        this.Render()
         this.#ResultSection.style.backgroundColor = 'wheat'
-
     }
 
-
-    Shuffle(){
-        let source = this.Words;
+    Shuffle() {
+        let source = this.Words
         this.Words = []
-        let wordsCount = source.length;
-        for(let i = wordsCount; i > 0; i--){
-            let number = Math.round(Math.random() * (--wordsCount));
-            var removed = source.splice(number, 1);
-            this.Words.push(removed[0]);
+        let wordsCount = source.length
+        for (let i = wordsCount; i > 0; i--) {
+            let number = Math.round(Math.random() * --wordsCount)
+            var removed = source.splice(number, 1)
+            this.Words.push(removed[0])
         }
     }
-
 }
-
--- index.js
+++ index.js
@@ -1,151 +1,132 @@
-function StartUp(){
-    LessonHolder.Get();
-    RenderDirectory();
+function StartUp() {
+    LessonHolder.Get()
+    RenderDirectory()
 }
 
+window.addEventListener('load', StartUp)
 
-window.addEventListener('load', StartUp);
-
-function RenderDirectory(){
-    var directory = LessonHolder.Get().Directory;
-    var directodyNode = document.getElementById('LessonDirectory');
-    RenderDirectoryNode(directodyNode, directory.Childrens);
-
+function RenderDirectory() {
+    var directory = LessonHolder.Get().Directory
+    var directodyNode = document.getElementById('LessonDirectory')
+    RenderDirectoryNode(directodyNode, directory.Childrens)
 }
 
 function RenderDirectoryNode(ulNode, childrenItems) {
-    if (!Array.isArray(childrenItems)) return;
+    if (!Array.isArray(childrenItems)) return
 
-    for(var i in  childrenItems)
-    {
+    for (var i in childrenItems) {
         let item = childrenItems[i]
-        if ( (item.IsLesson !== true)  && (item.IsLesson !== false)) continue;
+        if (item.IsLesson !== true && item.IsLesson !== false) continue
         let li = document.createElement('li')
-        ulNode.appendChild(li);
-        if (item.IsLesson){
+        ulNode.appendChild(li)
+        if (item.IsLesson) {
             li.innerHTML = item.Title
             li.dataset.lessonname = item.Lesson.Name
-            li.addEventListener('click', ()=> Navigator.OpenLessonInfo(item.Lesson))
-        }
-        else{
+            li.addEventListener('click', () => Navigator.OpenLessonInfo(item.Lesson))
+        } else {
             let span = document.createElement('span')
             span.innerText = item.Title
-            span.classList.add('directory')
-            
-            span.addEventListener('click', (e)=>{
-                li.dataset.collapse = (li.dataset.collapse === "1") ? "0" : "1"
-            });
-            li.appendChild(span)
-            li.dataset.collapse=  "0"//"1"
+            span.classList.add('explorer__item')
 
-            if (Array.isArray(item.Childrens)){
+            span.addEventListener('click', (e) => {
+                li.dataset.collapse = li.dataset.collapse === '1' ? '0' : '1'
+            })
+            li.appendChild(span)
+            li.dataset.collapse = '0' //"1"
+
+            if (Array.isArray(item.Childrens)) {
                 let ul = document.createElement('ul')
                 li.appendChild(ul)
                 RenderDirectoryNode(ul, item.Childrens)
             }
         }
-    
     }
 }
 
-function OnLessonClick(lesson){
-    console.log('Click on ' , lesson)
+function OnLessonClick(lesson) {
+    console.log('Click on ', lesson)
 }
 
-class LessonHolder{
-
+class LessonHolder {
     Lessons = []
-    Directory 
-    constructor(){
-
-        this.Directory = this.CreateDirectory('Root');
+    Directory
+    constructor() {
+        this.Directory = this.CreateDirectory('Root')
         var savedJson = localStorage.getItem('lessonHolder')
-        if(savedJson){
+        if (savedJson) {
             var savedLessonHolder = JSON.parse(savedJson)
-            this.Lessons = savedLessonHolder.Lessons || [];
-        }
-        else {
+            this.Lessons = savedLessonHolder.Lessons || []
+        } else {
             this.Lessons = []
         }
-
-        
     }
 
-    CreateDirectory(title){
+    CreateDirectory(title) {
         return {
-            Title : title,
-            IsLesson : false,
+            Title: title,
+            IsLesson: false,
             IsHidden: false,
-            IsNew :false,
-            Lesson : null,
-            Childrens : []
+            IsNew: false,
+            Lesson: null,
+            Childrens: [],
         }
     }
 
     CreateLessonInDirectory(lesson) {
         return {
-            Title : lesson.Name,
-            IsLesson : true,
+            Title: lesson.Name,
+            IsLesson: true,
             IsHidden: false,
-            IsNew :false,
-            Lesson : lesson,
-            Childrens : []
+            IsNew: false,
+            Lesson: lesson,
+            Childrens: [],
         }
     }
 
-    Save(){
+    Save() {}
 
-    }
-
-    AddToGroup(lesson){
-        
+    AddToGroup(lesson) {
         var pathParts = lesson.Directory.split('.')
 
-        if (pathParts.length == 1){
+        if (pathParts.length == 1) {
             this.Directory.Childrens[pathParts[0]] = CreateLessonInDirectory(lesson)
         }
 
-        var base = this.Directory;
+        var base = this.Directory
 
-        for(let i = 0; i < pathParts.length; i++){
-            let part = pathParts[i];
-            if (!base.Childrens[part]){
-                base.Childrens[part] = this.CreateDirectory(part);
+        for (let i = 0; i < pathParts.length; i++) {
+            let part = pathParts[i]
+            if (!base.Childrens[part]) {
+                base.Childrens[part] = this.CreateDirectory(part)
             }
-            base = base.Childrens[part];
+            base = base.Childrens[part]
         }
         base.Childrens[lesson.Name] = this.CreateLessonInDirectory(lesson)
     }
 
-    UpdateFromServer(){
-
+    UpdateFromServer() {
         console.log('serverData')
 
-        var serverData = Lesson.GetFromServer();
-        if (Array.isArray(serverData)){
-            for(var sourceLesson of serverData){
-
-                if (!this.Lessons[sourceLesson.Name]){
+        var serverData = Lesson.GetFromServer()
+        if (Array.isArray(serverData)) {
+            for (var sourceLesson of serverData) {
+                if (!this.Lessons[sourceLesson.Name]) {
                     this.Lessons[sourceLesson.Name] = new Lesson()
                 }
                 this.Lessons[sourceLesson.Name].LoadFromSource(sourceLesson)
 
-                this.AddToGroup(this.Lessons[sourceLesson.Name]);
-
+                this.AddToGroup(this.Lessons[sourceLesson.Name])
             }
-        }
-        else {
+        } else {
             console.warn('Serrver got something wrong')
         }
     }
 }
 
-
 LessonHolder.SingletonObject = null
-LessonHolder.Get = function(){
-
-    if (LessonHolder.SingletonObject) return LessonHolder.SingletonObject;
-    LessonHolder.SingletonObject = new LessonHolder();
-    LessonHolder.SingletonObject.UpdateFromServer();
-    return LessonHolder.SingletonObject;
+LessonHolder.Get = function () {
+    if (LessonHolder.SingletonObject) return LessonHolder.SingletonObject
+    LessonHolder.SingletonObject = new LessonHolder()
+    LessonHolder.SingletonObject.UpdateFromServer()
+    return LessonHolder.SingletonObject
 }
